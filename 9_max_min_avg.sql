c434e6c8ba4eac329ee70ab63ddaad3c8d8cdfe1
renbin.guo <949409706@qq.com>
2017-07-11 20:52:01 +0800

    add comment and rename 9.sql

--- 9_max_min_avg.sql
+++ 9_max_min_avg.sql
@@ -1,10 +1,11 @@
--- 求平均数
+-- 求平均数	 求Products表中所有商品的平均价格
 select AVG(prod_price) as avg_price from Products;
 
---统计所有顾客数(求行数的和)
+--统计所有顾客数(即统计一共有多少行)	求Customers表中顾客数
 select count(*) as num_cust from Customers;
+
 --	只统计有邮件的顾客数
-select count(cust_email)as num_cust1 from Customers;
+select count(cust_email)as n_email_cust from Customers;
 
 -- 求最贵的商品价格
 select max(prod_price) as max_price from Products;
@@ -12,6 +13,8 @@ select min(prod_price) as min_price from Products;
 
 --求订单20005所有购买的商品数
 select sum(quantity)as item_ordered from OrderItems where order_num= 20005;
+  -- 注意下面这个,它实际上求的是订单20005中买了几种商品
+select count(*) from OrderItems where order_num=20005;
 
 
 --聚集不同值 求所有价格不同商品的平均值
