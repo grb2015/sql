f369132f8fec9b2d890eed5167b4194b75f9c411
grb <949409706@qq.com>
2017-07-28 09:09:05 +0800

    m 16.sql

--- 16_update_del.sql
+++ 16_update_del.sql
@@ -1,12 +1,20 @@
---- 修改表中的数据。删除表中的某一行
+--- 修改表中的数据。删除/增加表中的某一行/列
 
 --- update:  给100000005增加电子邮件
     update Customers  set cust_email='kim@thetoystore.com'where cust_id = '1000000005';
     ---        取消给100000005增加电子邮件
     update Customers  set cust_email=NULL where cust_id = '1000000005';
+
 ---delete  删除100000006这一行
     delete from Customers where cust_id = '1000000006';
 
+--- 增加一列
+
+create table vendors_copy as select * from Vendors;
+alter table vendors_copy add vend_phone char(20);
+
+---删除一列
+alter table vendors_copy drop column vend_phone;
 
 
 
