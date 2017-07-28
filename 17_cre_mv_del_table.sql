28282b8bb2887ce38a4f50de34527c472189a2b5
grb <949409706@qq.com>
2017-07-28 09:12:47 +0800

    add 17.sql

---
+++ 17_cre_mv_del_table.sql
@@ -0,0 +1,19 @@
+--- 创建 、删除、重命名表
+
+---创建表 ，可以指定默认值
+CREATE TABLE Products2
+(
+  prod_id    char(10)      NOT NULL ,
+  vend_id    char(10)      NOT NULL ,
+  prod_name  char(255)     NOT NULL ,
+  prod_price decimal(8,2)  NOT NULL default 1.00,
+  prod_desc  text          NULL 
+);
+
+--- 删除表
+drop table Customers;
+---重命名表
+
+rename table CustCopy to Customers;
+
+
