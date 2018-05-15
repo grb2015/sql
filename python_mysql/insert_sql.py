5705bb787f6497b17dbe750631f27b381d905bf5
guorenbin <949409706@qq.com>
2018-05-15 21:37:30 +0800

    use python to process sql

---
+++ python_mysql/insert_sql.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+ 
+import pymysql
+ 
+# 打开数据库连接
+db = pymysql.connect("localhost","root","password","test_excel" )
+ 
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+ 
+# SQL 插入语句
+sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000),('renbin','guo',25,'M',5000)"""
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 如果发生错误则回滚
+   db.rollback()
+ 
+# 关闭数据库连接
+db.close()
